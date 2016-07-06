370398a7044f4e747d0759780807695d3ec44961
Dan Logajan <dlogajan@gmail.com>
2016-07-06 15:07:53 -0500

    Added ip for ipv4

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require("express"),
     port = process.env.port || 3000,
-    path = require("path");
+    ip = process.env.ip || "127.0.0.1",
+    path = require("path"),
+    getInfo = require("./getInfo");
 
 var app = express();
 
 app.use(express.static(path.join(__dirname + "/../")));
+app.use("/get-info", getInfo);
 
-app.listen(port);
+app.listen(port, ip);
 
 console.log("Running on port: " + port);
